dcea17db8c6ef7e4023973beda2fceddd236fdd5
Harish B S <92306237+harishbs3@users.noreply.github.com>
2022-08-13 16:43:01 +0530

    Add files via upload

---
+++ Band_name_generator.py
@@ -0,0 +1,8 @@
+from builtins import input
+
+
+print("Welcome to the Band Name Generator")
+city_name = input("Enter your city name: ")
+pet_name = input("Enter Your pet name: ")
+print(f"Tadaaaaa!    Your Band name is {city_name} {pet_name} !")
+
